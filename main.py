452236959c44f0774224f6b2b3c2cde163f2c462
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-02-08 11:16:45 -0600

    wtf

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import sys
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation}
+import matplotlib.animation as animation
 
 ON = 255
 OFF = 0
 vals = [ON, OFF]
 
+
 def randomGrid(N):
     """returns a grid of NxN random values"""
     return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+
 
 def addGlider(i, j, grid):
     """adds a glider with top left cell at (i, j)"""
@@ -23,6 +25,7 @@ def addGlider(i, j, grid):
                        [255,  0, 255],
                        [0,  255, 255]])
     grid[i:i+3, j:j+3] = glider
+
 
 def update(frameNum, img, grid, N):
     # copy grid since we require 8 neighbors for calculation
@@ -36,37 +39,40 @@ def update(frameNum, img, grid, N):
     return img,
 
 # main() function
+
+
 def main():
     # Command line args are in sys.argv[1], sys.argv[2] ..
     # sys.argv[0] is the script name itself and can be ignored
     # parse arguments
     parser = argparse.ArgumentParser(
-        description = "Runs Conway's Game of Life system.py.")
+        description="Runs Conway's Game of Life system.py.")
     # TODO: add arguments
 
     # set grid size
-    N=100
+    N = 100
 
     # set animation update interval
-    updateInterval=50
+    updateInterval = 50
 
     # declare grid
-    grid=np.array([])
+    grid = np.array([])
     # populate grid with random on/off - more off than on
-    grid=randomGrid(N)
+    grid = randomGrid(N)
     # Uncomment lines to see the "glider" demo
     # grid = np.zeros(N*N).reshape(N, N)
     # addGlider(1, 1, grid)
 
     # set up animation
-    fig, ax=plt.subplots()
-    img=ax.imshow(grid, interpolation = 'nearest')
-    ani=animation.FuncAnimation(fig, update, fargs = (img, grid, N, ),
-                                  frames = 10,
-                                  interval = updateInterval,
-                                  save_count = 50)
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                  frames=10,
+                                  interval=updateInterval,
+                                  save_count=50)
 
     plt.show()
+
 
 # call main
 if __name__ == '__main__':
