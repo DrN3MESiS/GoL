086d4b9f0d02fb8b723d6cb11d9df91b7fed65ac
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-02-08 19:01:56 -0600

    Added Rules

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import copy
 
 ON = 255
 OFF = 0
 vals = [ON, OFF]
+GRID = np.array([])
+GRID_SIZE = 100
 
 
 def randomGrid(N):
@@ -32,13 +35,128 @@ def update(frameNum, img, grid, N):
     # and we go line by line
     newGrid = grid.copy()
     # TODO: Implement the rules of Conway's Game of Life
+    newGrid = ApplyRules(grid)
 
     # update data
     img.set_data(newGrid)
     grid[:] = newGrid[:]
+
+    print("> Update")
     return img,
 
 # main() function
+
+
+def ApplyRules(grid: GRID) -> GRID:
+    updatedGrid = np.zeros(GRID_SIZE*GRID_SIZE).reshape(GRID_SIZE, GRID_SIZE)
+    tempGrid = grid.copy()
+
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            curItem = tempGrid[i][j]
+
+            # DEAD
+            if curItem == 0:
+                neiAm = 0
+                # Calculate Neighbours
+
+                # Down
+                if i != 0:
+                    if tempGrid[i-1][j] >= 255:
+                        neiAm += 1
+
+                # Up
+                if i != (GRID_SIZE-1):
+                    if tempGrid[i+1][j] >= 255:
+                        neiAm += 1
+
+                # Left
+                if j != 0:
+                    if tempGrid[i][j-1] >= 255:
+                        neiAm += 1
+
+                # Right
+                if j != (GRID_SIZE-1):
+                    if tempGrid[i][j+1] >= 255:
+                        neiAm += 1
+
+                # Down Left
+                if i != 0 and j != 0:
+                    if tempGrid[i-1][j-1] >= 255:
+                        neiAm += 1
+
+                # Down Right
+                if i != 0 and j != (GRID_SIZE-1):
+                    if tempGrid[i-1][j+1] >= 255:
+                        neiAm += 1
+
+                # Up Right
+                if i != (GRID_SIZE-1) and j != (GRID_SIZE-1):
+                    if tempGrid[i+1][j+1] >= 255:
+                        neiAm += 1
+
+                # Up Left
+                if i != (GRID_SIZE-1) and j != 0:
+                    if tempGrid[i+1][j-1] >= 255:
+                        neiAm += 1
+
+                if neiAm == 3:
+                    updatedGrid[i][j] = 255
+                    continue
+
+            # ALIVE
+            if curItem >= 255:
+                neiAm = 0
+
+                # Calculate Neighbours
+                if i != 0:
+                    if tempGrid[i-1][j] >= 255:
+                        neiAm += 1
+
+                if i != (GRID_SIZE-1):
+                    if tempGrid[i+1][j] >= 255:
+                        neiAm += 1
+
+                if j != 0:
+                    if tempGrid[i][j-1] >= 255:
+                        neiAm += 1
+
+                if j != (GRID_SIZE-1):
+                    if tempGrid[i][j+1] >= 255:
+                        neiAm += 1
+
+                if i != 0 and j != 0:
+                    if tempGrid[i-1][j-1] >= 255:
+                        neiAm += 1
+
+                if i != 0 and j != (GRID_SIZE-1):
+                    if tempGrid[i-1][j+1] >= 255:
+                        neiAm += 1
+
+                if i != (GRID_SIZE-1) and j != (GRID_SIZE-1):
+                    if tempGrid[i+1][j+1] >= 255:
+                        neiAm += 1
+
+                if i != (GRID_SIZE-1) and j != 0:
+                    if tempGrid[i+1][j-1] >= 255:
+                        neiAm += 1
+
+                # RULES
+
+                # Rule 1
+                if neiAm < 2:
+                    updatedGrid[i][j] = 0
+                    continue
+                # Rule 2
+                if neiAm == 2 or neiAm == 3:
+                    updatedGrid[i][j] = 255
+                    continue
+                # Rule 3
+                if neiAm > 3:
+                    updatedGrid[i][j] = 0
+                    continue
+
+    return updatedGrid
 
 
 def main():
@@ -49,24 +167,21 @@ def main():
         description="Runs Conway's Game of Life system.py.")
     # TODO: add arguments
 
-    # set grid size
-    N = 100
-
     # set animation update interval
     updateInterval = 50
 
     # declare grid
     grid = np.array([])
     # populate grid with random on/off - more off than on
-    grid = randomGrid(N)
+    # grid = randomGrid(N)
     # Uncomment lines to see the "glider" demo
-    # grid = np.zeros(N*N).reshape(N, N)
-    # addGlider(1, 1, grid)
+    grid = np.zeros(GRID_SIZE*GRID_SIZE).reshape(GRID_SIZE, GRID_SIZE)
+    addGlider(1, 1, grid)
 
     # set up animation
     fig, ax = plt.subplots()
     img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, GRID_SIZE, ),
                                   frames=10,
                                   interval=updateInterval,
                                   save_count=50)
